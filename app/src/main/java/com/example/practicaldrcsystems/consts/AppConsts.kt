48f6de09ddd2ae0737507082c403457a0f987332
Harsh A. Singh <harshsingh97.aks@gmail.com>
2023-02-02 21:13:24 +0530

    basic setup started

---
+++ app/src/main/java/com/example/practicaldrcsystems/consts/AppConsts.kt
@@ -0,0 +1,5 @@
+package com.example.practicaldrcsystems.consts
+
+object AppConsts {
+    const val TOTAL_AMOUNT = 80000
+}
--- app/src/main/java/com/example/practicaldrcsystems/views/MainActivity.kt
+++ app/src/main/java/com/example/practicaldrcsystems/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.practicaldrcsystems
+package com.example.practicaldrcsystems.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.practicaldrcsystems.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
